901fe6f068f9c38d69c0c1ddbd328b6a104983c3
itschool <itschool@tou.edu.kz>
2024-10-07 20:57:57 +0500

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    define("ABCD", "Hello World!");
+    echo "Hello World!";
+    // echo "abcd";
+    $a = "abcd";
+?>
+<h1><?= $a ?></h1>
+<?php
+    $a = "zxcv";
+?>
+<h1><?php echo $a # UNIX ?></h1>
+<?php
+    /*
+        1
+        2
+        3
+        4
+    */
+    echo ABCD;
+?>
